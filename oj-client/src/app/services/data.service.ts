b9919ed21dad08b141a4639b5a68083282a6652d
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-07-25 14:04:02 -0300

    fix-projects: Feat - Add apiBase address in 'dataService'

--- oj-client/src/app/services/data.service.ts
+++ oj-client/src/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Problem } from "../models/problem.model";
 import { PROBLEMS } from "../mock-problems";
 import { Http, Response, Headers } from '@angular/http';
-import { BehaviorSubject} from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/toPromise';
 
@@ -11,11 +11,12 @@ import 'rxjs/add/operator/toPromise';
 export class DataService {
 
   private problemsSource = new BehaviorSubject<Problem[]>([]);
+  private readonly apiBase = "http://localhost:3000";
 
   constructor(private http: Http) { }
 
   getProblems(): Observable<Problem[]> {
-    this.http.get("api/v1/problems")
+    this.http.get(`${this.apiBase}/api/v1/problems`)
       .toPromise()
       .then((res: Response) => {
         this.problemsSource.next(res.json());
@@ -26,15 +27,15 @@ export class DataService {
   }
 
   getProblem(id: number): Promise<Problem> {
-    return this.http.get(`api/v1/problems/${id}`)
-                     .toPromise()
-                     .then((res: Response) => res.json())
-                     .catch(this.handleError);
+    return this.http.get(`${this.apiBase}/api/v1/problems/${id}`)
+      .toPromise()
+      .then((res: Response) => res.json())
+      .catch(this.handleError);
   }
 
   addProblem(problem: Problem): Promise<Problem> {
     let headers = new Headers({ 'content-type': 'application/json' });
-    return this.http.post('/api/v1/problems', problem, headers)
+    return this.http.post(`${this.apiBase}/api/v1/problems`, problem, headers)
       .toPromise()
       .then((res: Response) => {
         this.getProblems();
@@ -44,14 +45,14 @@ export class DataService {
   }
 
   buildAndRun(data): Promise<Object> {
-    let headers = new Headers({'content-type': 'application/json'});
-    return this.http.post('/api/v1/build_and_run', data, headers)
-                    .toPromise()
-                    .then((res: Response) => {
-                      console.log(res);
-                      return res.json();
-                    })
-                    .catch(this.handleError);
+    let headers = new Headers({ 'content-type': 'application/json' });
+    return this.http.post(`${this.apiBase}/api/v1/build_and_run`, data, headers)
+      .toPromise()
+      .then((res: Response) => {
+        console.log(res);
+        return res.json();
+      })
+      .catch(this.handleError);
   }
 
   // error hanlder
