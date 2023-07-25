c9fbbc1e5f7c155dd5a52377cdb7a1eaeb1872ef
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-07-25 16:41:25 -0300

    fix-projects: Chore - Adjust 'launcher' shell file

--- launcher.sh
+++ launcher.sh
@@ -2,20 +2,20 @@
 fuser -k 3000/tcp
 fuser -k 5000/tcp
 
-service redis_6379 start
+service redis start
 cd ./oj-server
-npm install
-nodemon server.js &
+npm i
+npm run dev &
 cd ../oj-client
-npm install
-ng build --watch &
+npm i
+npm start &
 cd ../executor
 pip install -r requirements.txt
-python executor_server.py &
+python3 executor_server.py &
 
 echo "=================================================="
-read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
+read -p "Pressione [ENTER] para fechar os processos." PRESSKEY
 
 fuser -k 3000/tcp
 fuser -k 5000/tcp
-service redis_6379 stop
+service redis stop
